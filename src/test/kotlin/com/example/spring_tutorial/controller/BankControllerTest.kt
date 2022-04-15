860702982741797d3e5cf4965c62977533b91f97
UsmanKhanAbasi <usman.khan.abasi@wolt.com>
2022-04-15 16:59:08 +0200

    Add test case for controller layer for "Bank"

---
+++ src/test/kotlin/com/example/spring_tutorial/controller/BankControllerTest.kt
@@ -0,0 +1,35 @@
+package com.example.spring_tutorial.controller
+
+import com.example.spring_tutorial.mock.MockBankDataSource
+import com.example.spring_tutorial.model.Bank
+import com.example.spring_tutorial.service.BankService
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+
+class BankControllerTest {
+
+    private lateinit var controller: BankController
+
+    @Mock
+    private var bankService = mock<BankService>()
+
+    @BeforeEach
+    fun setUp(){
+        this.controller = BankController(bankService)
+    }
+
+    @Test
+    fun `test controller returns data`(){
+        Mockito.`when`(bankService.getBanks()).thenReturn(
+            listOf(
+                Bank(1234, 12.0, 10)
+            )
+        )
+
+        var banks = controller.getBanks()
+//        TODO("Add assertions")
+    }
+}
